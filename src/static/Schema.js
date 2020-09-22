0d741fdb872a0e7dc4864439f3412a403300deba
Byron Hall <bjhall2@crimson.ua.edu>
2020-09-22 11:12:08 -0500

    json forms

--- src/static/Schema.js
+++ src/static/Schema.js
@@ -3,7 +3,22 @@ import configSettings from '../../static/config.json';
 export default {
   groups: [
     {
-      branches: configSettings.branches,
+      foo: 'bar',
+    },
+    {
+      l: 'Description',
+      fields: [
+        { foo: 'bar' },
+        { foo: 'bar' },
+        {
+          model: 'ServiceBranch',
+          values: configSettings.branches,
+        },
+        { foo: 'bar' },
+      ],
+    },
+    {
+      bar: 'foo',
     },
   ],
 };
